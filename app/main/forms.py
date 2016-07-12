c7b3b8b5f3c977105114393928a8892c2b67c91e
hamun <commentatorbot@gmail.com>
2016-07-12 12:36:06 +0200

    updated with newest version of flask

---
+++ app/main/forms.py
@@ -0,0 +1,74 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField, IntegerField
+from wtforms.validators import Required, Length, Email, Regexp
+from wtforms import ValidationError
+from flask.ext.pagedown.fields import PageDownField
+from ..models import Role, User
+
+#insert form
+class InsertForm(Form):
+    #what is the name of the product?
+    name = StringField('Product name', validators=[Required()])
+    #What is the sales price?
+    salesPrice = IntegerField('Købspris', validators=[Required()])
+    #how many did you brought
+    amount = IntegerField('Antal', validators=[Required()])
+    #what is the purchased price?
+    purchasedPrice = IntegerField('Salspris', validators=[Required()])
+    #supplier = StringField('Name of the supplier', validators=[Required()])
+    #desc = StringField('The description of the product')
+    #image = FileField('image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    #brand = StringField('Name of the brand', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class EditProfileForm(Form):
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+
+class EditProfileAdminForm(Form):
+    email = StringField('Email', validators=[Required(), Length(1, 64),
+                                             Email()])
+    username = StringField('Username', validators=[
+        Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                          'Usernames must have only letters, '
+                                          'numbers, dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 64)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditProfileAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name)
+                             for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+
+class PostForm(Form):
+    body = PageDownField("What's on your mind?", validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+class CommentForm(Form):
+    body = StringField('Enter your comment', validators=[Required()])
+    submit = SubmitField('Submit')
